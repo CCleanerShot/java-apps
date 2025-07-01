fe627dbba28dee9cd0ff3cdeba7140c533c59221
CCleanerShot <31040856+CCleanerShot@users.noreply.github.com>
2025-07-01 09:17:35 -0700

    core: implement valid number validation via Lohh's algorithm

--- apps/credit-card-validator/src/main/java/com/andrylat/dev/CreditCard/CreditCard.java
+++ apps/credit-card-validator/src/main/java/com/andrylat/dev/CreditCard/CreditCard.java
@@ -3,14 +3,25 @@ package com.andrylat.dev.CreditCard;
 import com.andrylat.dev.Enums.CreditCardProvider;
 
 public class CreditCard {
-    final String Input;
+    final String InputOriginal;
     final Long InputNumeric;
     public boolean IsValid = false;
     public CreditCardProvider Provider;
 
     public CreditCard(String _Input) {
-        Input = _Input;
+        InputOriginal = _Input;
         InputNumeric = ParseInput();
+    }
+
+    /**
+     * TODO: put in utility class, but too lazy right now.
+     * Helper function that gets the digit of a number by position from the left.
+     * @param digit
+     * @return
+     */
+    final static Integer GetDigit(Long digit, int position) {
+        int result = (int)((digit / (1 * (Math.pow(10, Long.toString(digit).length() - 1 - position)))) % 10);
+        return result;
     }
 
     /**
@@ -28,7 +39,30 @@ public class CreditCard {
      * @return boolean
      */
     public final static boolean IsValidNumber(CreditCard card) {
-        return false;
+        int sum = 0;
+
+        String string = Long.toString(card.InputNumeric);
+        // even digits, multiple by 2, add result of digits if 2 digit, and return result
+        // odd digits, return result
+        // ignore last number as it is the checkDigit
+        for (int i = 0; i < string.length() - 1; i++) {
+            Integer number = GetDigit(card.InputNumeric, i);
+
+            if(i % 2 == 0) {
+                number = number * 2;
+
+                if(number >= 10) {
+                    sum += (number / 10) + (number % 10);
+                    continue;
+                }
+            }
+
+            sum += number;
+        }
+        
+        // check digit is the value needed to make the result a multiple of 10
+        int checkDigit = 10 - (sum % 10);
+        return checkDigit == GetDigit(card.InputNumeric, Long.toString(card.InputNumeric).length() - 1);
     }
     
     /**
@@ -47,7 +81,7 @@ public class CreditCard {
         Long result = -1L;
 
         try {
-            result = Long.valueOf(Input.trim().replace(" ", ""));
+            result = Long.valueOf(InputOriginal.trim().replace(" ", ""));
         } catch (NumberFormatException err) {
             
         }
--- apps/credit-card-validator/src/test/java/com/andrylat/dev/CreditCardValidatorTest.java
+++ apps/credit-card-validator/src/test/java/com/andrylat/dev/CreditCardValidatorTest.java
@@ -30,19 +30,19 @@ public class CreditCardValidatorTest {
     //     assertEquals(validator.CardValid(1), false, "CardValid(1) should equal false");
     // }
 
-    // @Test
-    // void creditCardValidatorValidNumberWorks() {
-    //     String[] validInputs = new String[] {"2817291823901827", "1827 2843 2019 2892", "2817  2187 2857 4920", "1762 2948 2010 1752"};
-    //     String[] invalidInputs = new String[] {"2817238118271728sjw", "2817 2834 4781 4912s", "0182 3821 4711 132", "a2918 2847 2716 1627"};
+    @Test
+    void creditCardValidatorValidNumberWorks() {
+        String[] validInputs = new String[] {"5457623898234113", "3918 4829 2716 4728", "1238 2717 4858 1922", "5829 4728 4716 4786", "4242 4242 4242 4242"};
+        String[] invalidInputs = new String[] {"2817238118271728sjw", "2817 2834 4781 4912", "0182 3821 4711 2131", "0000 0000 0000 0000", "9999 9999 9999 9999"};
         
-    //     for (String input : validInputs) {
-    //         CreditCard card = new CreditCard(input);
-    //         assertEquals(CreditCard.IsValidNumber(card), true);
-    //     }
+        for (String input : validInputs) {
+            CreditCard card = new CreditCard(input);
+            assertEquals(CreditCard.IsValidNumber(card), true);
+        }
 
-    //     for (String input : invalidInputs) {
-    //         CreditCard card = new CreditCard(input);
-    //         assertEquals(CreditCard.IsValidNumber(card), false);
-    //     }
-    // }
+        for (String input : invalidInputs) {
+            CreditCard card = new CreditCard(input);
+            assertEquals(CreditCard.IsValidNumber(card), false);
+        }
+    }
 }
